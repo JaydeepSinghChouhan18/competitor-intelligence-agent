80bb53291076f728a190774504d25fff549a33a9
Jaydeep Singh Chouhan <jschauhan7773@gmail.com>
2026-06-14 13:37:47 +0530

    fix render deployment

--- rag/vectordb.py
+++ rag/vectordb.py
@@ -1,28 +1,6 @@
-from langchain_community.vectorstores import Chroma
-from langchain_huggingface import HuggingFaceEmbeddings
-
-from config import CHROMA_DB_PATH
-
-
-EMBEDDING_MODEL = (
-    "sentence-transformers/all-MiniLM-L6-v2"
-)
-
-
-def get_embedding_model():
-
-    return HuggingFaceEmbeddings(
-        model_name=EMBEDDING_MODEL
-    )
-
+class DummyDB:
+    def similarity_search(self, query, k=3):
+        return []
 
 def get_vector_db():
-
-    embeddings = get_embedding_model()
-
-    db = Chroma(
-        persist_directory=CHROMA_DB_PATH,
-        embedding_function=embeddings
-    )
-
-    return db
+    return DummyDB()
